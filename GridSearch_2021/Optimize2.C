8875f64de18365ce66e102df437fec2b14cb7666
Karol Mateusz Sowa <karol.mateusz.sowa@cern.ch>
2021-05-12 23:05:24 +0200

    l

--- GridSearch_2021/Optimize2.C
+++ GridSearch_2021/Optimize2.C
@@ -21,21 +21,37 @@ void Optimize2(){
 	std::cout << std::endl << "==>TMVAClassification: SECOND CLASSIFIER" << std::endl;
 
 	std::vector<TString> variables2 = {
-		"K_Plus_D_IPCHI2_OWNPV",
-		"Pi_Minus1_D_IPCHI2_OWNPV",
-		"Pi_Minus2_D_IPCHI2_OWNPV",
-		"Pi_Plus_IPCHI2_OWNPV", 
-		"KS0_ENDVERTEX_CHI2",
-		"KS0_IPCHI2_OWNPV",
-		"KS0_PT",
-		"Pi_Plus_KS0_IPCHI2_OWNPV",
-		"Pi_Minus_KS0_IPCHI2_OWNPV",
-		"KS0_DIRA_ORIVX",
-		"KS0_ORIVX_CHI2",
-		"KS0_FDCHI2_OWNPV",
+		"B_ENDVERTEX_CHI2",
+        "B_OWNPV_CHI2",
+        "B_IPCHI2_OWNPV",
+        "B_DIRA_OWNPV",
+        "B_PT",
+        "B_FDCHI2_OWNPV",
+        "D0_IPCHI2_OWNPV",
+        "D0_IP_OWNPV",
+        "D0_DIRA_ORIVX",
+        "D0_ENDVERTEX_CHI2",
+        "D0_FDCHI2_OWNPV",
+        "D0_DIRA_OWNPV",
+        "D0_FD_OWNPV",
+        "D0_FD_ORIVX",
+        "D0_FDCHI2_ORIVX",
+        "D0_OWNPV_CHI2",
+        "maxD_GhostProb",
+        "minIPchi2_D_childs",
+        "D0_Dist_z",
+//		"deltaR_Ks0Pi", 				//później
+//		"KS0_CosTheta",
+//		"K_Plus_D_PIDK",
+//		"K_Minus_D_PIDK",
+//		"Pi_Minus_D_PIDK",
+//		"Pi_Plus_KS0_PIDK",
+//		"Pi_Minus_KS0_PIDK",
+//		"Pi_Plus_PIDK" 
+
 	};
 	std::vector<TString> spectators2 = {};
-	TString outfileName2 = "/home/student/Project/B2DK0Pi/ANALIZA/TMVA_cascade1_and_2.root";
+	TString outfileName2 = "/home/sowrol/repo/TMVA_cascade1_OTP.root";
 	TFile* outputFile2 = TFile::Open(outfileName2, "RECREATE");
 
 	TMVA::Factory* factory2 = new TMVA::Factory("TMVAClassification", outputFile2,"!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification");
@@ -55,9 +71,11 @@ void Optimize2(){
 	TChain* background2  = new TChain("background2");
 	TChain* inputTree2  = new TChain("inputTree2");
 
-	signal2->Add("/home/student/Project/B2DK0Pi/B2DK0Pi_2012MC_sig.root/DecayTree"); 		
-	background2->Add("/home/student/Project/B2DK0Pi/B2D3Pi_2016MC_bkg.root/DecayTree");		
-	inputTree2->Add("/home/student/Project/B2DK0Pi/ANALIZA/MyBDT_cascade1.root/DecayTree");
+	signal2->Add("/data4/muchaa/Jan2021/NTUPLE/Bd2DK0Pi_2012MC_slim.root/DecayTree"); 
+	signal2->Add("/data4/muchaa/Jan2021/NTUPLE/Bd2DK0K_2012MC_slim.root/DecayTree");
+	//background->Add("/home/student/Project/B2DK0Pi/B2D3Pi_2016MC_bkg.root/DecayTree");			//Jako input dać MC signal 2012
+	background2->Add("/data4/muchaa/Jan2021/B2DK0sPi_2016up_slim_B_Dsidebans.root/DecayTree");		//Przetestować dla obydwu
+	inputTree2->Add("/data4/muchaa/Jan2021/NTUPLE/B2DK0sPi_2016up_slim_v4.root/DecayTree"); 
 
 	TString myfile2 =  "/home/student/Project/B2DK0Pi/ANALIZA/MyBDT_cascade1_and_2.root";
 
