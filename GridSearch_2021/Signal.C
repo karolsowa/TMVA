c69b6103d84e6730bc945566aeb4067638c874c5
Karol Mateusz Sowa <karol.mateusz.sowa@cern.ch>
2021-06-01 23:09:43 +0200

    l

--- GridSearch_2021/Signal.C
+++ GridSearch_2021/Signal.C
@@ -12,9 +12,9 @@ void Signal(bool mode = 1){
 	//gROOT->Reset();
     // gROOT->ProcessLine(".L lhcbstyle.C");
     // lhcbStyle();
-	TString GS_filename = "/home/sowrol/repo/MyBDT_cascade1_job2_GS.root";
-	TString HPO_filename = "/home/sowrol/repo/MyBDT_cascade1_job2_HPO.root";
-	TString OTP_filename = "/home/sowrol/repo/MyBDT_cascade1_job2_OTP.root";
+	TString GS_filename = "/home/sowrol/repo/BD_classifier/MyBDT_cascade1_job2_GS.root";
+	TString HPO_filename = "/home/sowrol/repo/BD_classifier/MyBDT_cascade1_job2_HPO.root";
+	TString OTP_filename = "/home/sowrol/repo/BD_classifier/MyBDT_cascade1_job2_OTP.root";
 	TFile *GS_File = TFile::Open(GS_filename);
 	TFile *HPO_File = TFile::Open(HPO_filename);
 	TFile *OTP_File = TFile::Open(OTP_filename);
