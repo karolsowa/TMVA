d0075621c0f6e8f1365ec248286df51e407904bd
Karol Mateusz Sowa <karol.mateusz.sowa@cern.ch>
2021-06-01 21:48:37 +0200

    l

--- GridSearch_2021/ClassifierCompare.C
+++ GridSearch_2021/ClassifierCompare.C
@@ -7,10 +7,11 @@
 #include <TTreeReaderArray.h>
 #include <string>
 
-gROOT->ProcessLine(".L lhcbstyle.C");
-lhcbStyle();
+
 void ClassifierCompare(bool mode = 1){
 	//gROOT->Reset();
+    gROOT->ProcessLine(".L lhcbstyle.C");
+    LHCbStyle::lhcbStyle();
 	TString GS_filename = "/home/sowrol/repo/MyBDT_cascade1_job2_GS.root";
 	TString HPO_filename = "/home/sowrol/repo/MyBDT_cascade1_job2_HPO.root";
 	//TString OTP_filename = "/home/sowrol/repo/MyBDT_cascade1_job2_OTP.root";
